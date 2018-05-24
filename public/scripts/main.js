e0ea7864b51be014e6537983409fb0bacc910464
Jessica <jmc617@gmail.com>
2018-05-24 14:07:45 -0400

    inputs added

--- public/scripts/main.js
+++ public/scripts/main.js
@@ -4,3 +4,18 @@ var days = document.getElementsByClassName('d')
 var reserve = document.getElementById('reserve')
 var reserveComplete = document.getElementById('reservecomplete')
 var reserveBtn = document.getElementById('reservebtn')
+
+console.log(days[0]);
+console.log(days[0].value);
+console.log(days[0].innerHTML);
+console.log(days);
+
+function inputDay(){
+  for (var i = 0; i < days.length; i++) {
+    days[i].addEventListener('click',function(){
+      //   inputDay(i)
+      // console.log(days[i]);
+      // reserve.value += days[i].value+" "
+    })
+  }
+}
