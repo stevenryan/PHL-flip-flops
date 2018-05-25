e82a8c48166cb58adae90c4c3cd42785c6a16b67
Steven Ng <stevenrng@gmail.com>
2018-05-25 15:58:34 -0400

    added Email functionality to contact page

--- app.rb
+++ app.rb
@@ -24,3 +24,23 @@ end
 get "/contact" do
 erb :contact
 end
+
+get "/sendEmail" do
+  @email = params[:email]
+  @subject = params[:subject]
+  @content = params[:content]
+
+  from = SendGrid::Email.new(email: @email)
+  to = SendGrid::Email.new(email: 'stevenrng@gmail.com')
+  subject = @subject
+  content = SendGrid::Content.new(type: 'text/plain', value: @content)
+  mail = SendGrid::Mail.new(from, subject, to, content)
+
+  sg = SendGrid::API.new(api_key: ENV['SENDGRID_API_KEY'])
+  response = sg.client.mail._('send').post(request_body: mail.to_json)
+  # puts response.status_code
+  # puts response.body
+  # puts response.parsed_body
+  # puts response.headers
+  erb :contact
+end
